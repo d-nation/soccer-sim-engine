17cd9327f762409b1b0872e4a02fd809f4da7baa
Dustin Nation <d.r.nation@gmail.com>
2014-07-29 10:57:28 -0500

    Fixed the y coordinate range
    Updated the doc with the 1-6 range

--- game_state_engine/game-state-doc.js
+++ game_state_engine/game-state-doc.js
@@ -14,7 +14,7 @@ var state = {
             "player": null //ID of player with possession.  null=No player has possession.
         },
         "pitch": {
-            "current": [5,4], //uses grid system ranges [1-8, 1-4]
+            "current": [5,4], //uses grid system ranges [1-8, 1-6]
             "status": 0 //0=Kickoff, 1=In Play, 2=Free Kick, 3=ThrowIn, 4=GoalKick, 5=CornerKick, 6=Penalty, 7=Goal, 8=Out of Touch(ThrowIn), 9=Out of Touch(GoalKick)
         },
         "previousPlay": 0 //0=Kickoff, 1=Pass, 2=MoveWithBall, 3=Shot, 4=Foul, 5=Free Kick, 6=ThrowIn, 7=GoalKick, 8=CornerKick, 9=Penalty, 10=Goal, 11=Out of Touch(ThrowIn), 12=Out of Touch(GoalKick)
